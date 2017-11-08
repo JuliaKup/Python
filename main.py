7bd4d6ffb895391c4e1245a40e17850efb2369e5
Julia <yuliya.wolf@gmail.com>
2017-11-08 09:18:56 +0300

    update isomorphism

--- main.py
+++ main.py
@@ -1,43 +1,42 @@
 import numpy as np
 
-def degree(vertex):
-    return vertex.count(1)
+def degreeIn(graph, vertex):
+    return [graph[i][vertex] for i in range(len(graph))].count(1)
 
-def check(A, B, AIsms = None):
+def degreeOut(graph, vertex):
+    return graph[vertex].count(1)
+
+def check(A, B):
     if len(A) != len(B):
         return False
 
     vcount = len(A)
-    isoMap = []
-
-    if AIsms: AIsms = [[] for _ in range(vcount)]
-
+    isoMap = [None] * vcount
     used = [False] * vcount
-    deg1 = [degree(v) for v in A]
-    deg2 = [degree(v) for v in B]
 
     isIsm = False
 
-    v2 = 0
+    def findIsomorphism(v1):
+        if v1 >= vcount:
+            return True
 
-    def findIsomorphism(v):
-        if v >= vcount:
-            isIsm = True
-            if AIsms: AIsms.append(isoMap)
-        else:
-            return
+        isCanMap = True
 
         for v2 in range(vcount):
-            if not used[v2] and deg1[v1] == deg2[v2]:
-        isCanMap = True
+            if not used[v2] and degreeIn(A, v1) == degreeIn(B, v2) and degreeOut(A, v1) == degreeOut(B, v2):
+                isCanMap = True
+                for i in range(v1):
+                    if A[i][v1] != B[isoMap[i]][v2] or A[v1][i] != B[v2][isoMap[i]]:
+                        isCanMap = False
+
         if isCanMap:
             used[v2] = True
             isoMap[v1] = v2
-            findIsomorphism(v1 + 1)
-            if isIsm and ...: return isIsm
+            isIsm = findIsomorphism(v1 + 1)
+            if isIsm: return isIsm
             used[v2] = False
-        pass
 
+    isIsm = findIsomorphism(0)
     return isIsm
 
 def main():
@@ -45,13 +44,13 @@ def main():
 
     A = []
     for i in range(n):
-        A.append([int(x) for x in input().split()])
+        A.append([int(x) for x in input()])
 
     m = int(input())
     B = []
     for i in range(m):
-        B.append([int(x) for x in input().split()])
+        B.append([int(x) for x in input()])
 
-    return check(A, B)
+    print(check(A, B))
 
 main()
